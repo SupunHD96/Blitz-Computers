3460f0cdf0cbcb07ebe5d62e9a5303a1df2a1425
vae97 <gakashekanayaka@gmail.com>
2021-10-01 21:37:57 +0530

    Init project

---
+++ app/src/App.js
@@ -0,0 +1,13 @@
+import { Card } from './Components/Card';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Blitz Computes</h1>
+      <Card/>
+    </div>
+  );
+}
+
+export default App;
---
+++ app/src/Components/Card.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export function Card(props) {
+    
+
+    return (
+        <div>
+            <h1>Card component</h1>
+        </div>
+    )
+}
